169b319b6fc2da46a0914aa11bb4c8639a89ea10
IvoVilas <sutiaplications@gmail.com>
2024-06-05 22:02:49 +0100

    Add swift to go back

--- TachiOSmi/Src/UI/View/ContentView.swift
+++ TachiOSmi/Src/UI/View/ContentView.swift
@@ -86,6 +86,20 @@ struct ContentView: View {
 
 }
 
+extension UINavigationController: UIGestureRecognizerDelegate {
+
+  override open func viewDidLoad() {
+    super.viewDidLoad()
+
+    interactivePopGestureRecognizer?.delegate = self
+  }
+
+  public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+    return viewControllers.count > 1
+  }
+
+}
+
 #Preview {
   ContentView()
 }
